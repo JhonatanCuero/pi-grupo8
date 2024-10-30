25fd6e142c627edd6920b0ec1695a5aee95fe332
JhonatanCuero <cuero.jhonatan@correounivalle.edu.co>
2024-10-30 00:44:36 -0500

    Extended the 3D Fox Model in the page

--- src/pages/biodiversidad/BiodiversityPage.jsx
+++ src/pages/biodiversidad/BiodiversityPage.jsx
@@ -10,7 +10,6 @@ const BiodiversityPage = () => {
       <div style={{ padding: '20px', textAlign: 'center' }}>
         <h1>Pérdida de Biodiversidad</h1>
         <p>La pérdida de biodiversidad se refiere a la desaparición de especies y ecosistemas a nivel global, lo que amenaza la estabilidad y resiliencia de los ecosistemas naturales.</p>
-        
         <div style={{ width: '100%', height: '600px' }}>
           <Canvas shadows>
             <OrbitControls />
