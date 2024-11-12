ffbf60a5325b9129a16646a7b05711fcedf9f698
santiago <santiago.rodriguez.silva@correounivalle.edu.co>
2024-11-11 21:19:12 -0500

    sensibilizacion

---
+++ src/World.jsx
@@ -0,0 +1,9 @@
+const World = (props) => {
+    return (
+        <div>
+            <h1>Hola</h1>
+            <p>{props.text}</p>
+        </div>
+    )
+}
+export default World;
--- src/pages/RoutesProject.jsx
+++ src/pages/RoutesProject.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Home";
 import App from "./App";
 import Menu from "./Menu";
-import DeforestationPage from './DeforestacionPage';  // Página de Deforestación
+import DeforestacionPage from "./Deforestation/DeforestacionPage";  // Página de Deforestación
 import ErosionPage from './Erosion/ErosionPage';  // Página de Erosión del Suelo
 import BiodiversityPage from './biodiversidad/BiodiversityPage';  // Página de Pérdida de Biodiversidad
 import UserPage from './UserPage';  // Página de Usuario
+import Sensibilization from './Deforestation/Sensibilization';
 
 const RoutesProject = () => {
   return (
@@ -24,10 +25,11 @@ const ContentWithConditionalMenu = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/deforestacion" element={<DeforestationPage />} />  
+        <Route path="/deforestacion" element={<DeforestacionPage />} />  
         <Route path="/erosion" element={<ErosionPage />} />  
         <Route path="/biodiversidad" element={<BiodiversityPage />} />  
         <Route path="/usuario" element={<UserPage />} />
+        <Route path="/Sensibilization" element ={<Sensibilization />} />
       </Routes>
     </>
   );
